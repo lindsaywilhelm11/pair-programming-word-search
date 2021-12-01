17bb6948d962906a9cb25385bdaeab3cd84bd869
lindsaywilhelm11 <lindsaywilhelm11@gmail.com>
2021-12-01 23:21:42 +0000

    completedd word search and added tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -18,7 +18,26 @@ describe("#wordSearch()", function() {
     ], 'FRANK')
 
     assert.isFalse(result);
+
+});
+  
+// VERTICAL TESTING
+    it("should return true if word is found vertically", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'LARRY')
+
+    assert.isTrue(result);
   });
+  
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
@@ -34,5 +53,5 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
-  });
+ });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,28 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    
+    return false;
 }
 
+const transpose = function (matrix) {
+    let transposedMatrix = [];
+    for (let j = 0; j < matrix[0].length; j++) {
+        let row = [];
+        for (let i = 0; i < matrix.length; i++){
+            row.push(matrix[i][j]);
+        }
+        transposedMatrix.push(row);
+    }
+    return transposedMatrix;
+};
+
 module.exports = wordSearch
+
+
+
+
